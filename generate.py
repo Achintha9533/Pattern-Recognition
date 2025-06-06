8578f16da0ac1f4ce1855f3b7a4732554c3f6d49
Achintha Perera <pereraachintha84@gmail.com>
2025-06-06 16:39:15 +0200

    created for image generation

---
+++ generate.py
@@ -0,0 +1,11 @@
+import torch
+
+@torch.no_grad()
+def generate(model, z0, device, steps=100):
+    model.eval()
+    z = z0.clone().to(device)
+    for i in range(steps):
+        t = torch.tensor(i / steps, device=device).repeat(z.shape[0])
+        v = model(z, t)
+        z = z + v / steps
+    return z
