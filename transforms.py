ad71e013f87b5d0facc9f52ccfd461d194c38e56
Achintha Perera <pereraachintha84@gmail.com>
2025-06-21 16:26:29 +0200

    Adjusted transform function to normalize and resize images

--- transforms.py
+++ transforms.py
@@ -1,10 +1,9 @@
 import torchvision.transforms as T
 
-image_size = (64, 64)
-
-transform = T.Compose([
-    T.ToPILImage(),
-    T.Resize(image_size),
-    T.ToTensor(),
-    T.Normalize(mean=[0.5], std=[0.5])
-])
+def get_transform():
+    return T.Compose([
+        T.ToPILImage(),
+        T.Resize((64, 64)),
+        T.ToTensor(),
+        T.Normalize(mean=[0.5], std=[0.5])
+    ])
