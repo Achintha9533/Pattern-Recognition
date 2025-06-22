b22e1e5562830ee88171dad56af765f658faac75
Achintha Perera <pereraachintha84@gmail.com>
2025-06-22 23:42:55 +0200

    Add some explainations

--- transforms.py
+++ transforms.py
@@ -1,9 +1,19 @@
 import torchvision.transforms as T
 
-def get_transform():
-    return T.Compose([
-        T.ToPILImage(),
-        T.Resize((64, 64)),
-        T.ToTensor(),
-        T.Normalize(mean=[0.5], std=[0.5])
+def get_transform(image_size):
+    """
+    Returns the image preprocessing transform for CT images.
+
+    Args:
+        image_size (tuple): Desired output image size (height, width).
+
+    Returns:
+        torchvision.transforms.Compose: Composed transformations.
+    """
+    transform_post_hu = T.Compose([
+        T.ToPILImage(),               # Convert numpy array to PIL Image for transformations
+        T.Resize(image_size),         # Resize to specified size
+        T.ToTensor(),                 # Convert PIL Image to torch tensor (C x H x W), scales [0,1]
+        # Normalization to [-1, 1] happens within load_dicom_image after HU windowing.
     ])
+    return transform_post_hu
